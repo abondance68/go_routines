af6e21e18d47bfdd22928d72205f0d37940e238e
LAURENT KNAUSS <laurentknauss@protonmail.com>
2023-04-22 16:50:29 +0200

    Updated producer_consumer.go

--- producer_consumer.go
+++ producer_consumer.go
@@ -1,4 +1,20 @@
- 
+/*
+he main function starts two goroutines using the go keyword: one for the producer function and 
+another for the consumer function. 
+The producer function writes to the done channel after it finishes sending five messages to the comm channel.
+The consumer function reads from the comm channel indefinitely
+until it reads a false value, at which point it breaks out of the loop.
+After starting the two goroutines, the main function blocks on the <-done statement, waiting for a value to be written to the done channel.
+Once it receives a value, it proceeds to print "All Done!".
+
+
+
+
+
+
+
+
+*/
 
 
 
